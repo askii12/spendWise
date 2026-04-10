5641e6111b77e65804bc3a5e0751d12b1a7685e2
DeresiuPhoi <mussepovn@gmail.com>
2026-04-11 04:03:13 +0500

    certificate

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://YOUR-VERCEL-DOMAIN.vercel.app"],
+    origin: ["http://localhost:5173", "https://spend-wise-vh21.vercel.app/"],
   }),
 );
 app.use(express.json());
