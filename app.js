006419037324f4be2d139b4a646ead189267690c
Nurlan Mussepov <mussepovn@gmail.com>
2026-04-11 18:14:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://spend-wise-zdt8.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
