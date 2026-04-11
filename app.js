79a6234c86b6c34bbcd2cc368269d35c7436ff38
Nurlan Mussepov <mussepovn@gmail.com>
2026-04-11 17:30:00 +0500

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://spend-wise-vh21.vercel.app/"],
+    origin: ["http://localhost:5173", "https://spend-wise-5vd2.vercel.app/"],
   }),
 );
 app.use(express.json());
